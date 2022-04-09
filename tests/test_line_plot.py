fc287f38a24883842bc2c28066dd8321ce32cf70
sasiburi <zhouyilin512@gmail.com>
2022-04-09 15:08:50 -0700

    added input type check

--- tests/test_line_plot.py
+++ tests/test_line_plot.py
@@ -13,6 +13,14 @@ name = "name of the plot"
 def test_line_plot_type():
     assert isinstance(line_plot(Ks, mean, std, x, y, name), matplotlib.figure.Figure)
     
+def test_line_plot_input_type():
+    assert isinstance(x, str)
+    assert isinstance(y, str)
+    assert isinstance(name, str)
+    assert isinstance(Ks, int)
+    assert isinstance(mean, np.ndarray)
+    assert isinstance(std, np.ndarray)
+    
 @pytest.mark.mpl_image_compare(baseline_dir='baseline',
                                filename='test_line_plot.png')
 def test_line_plot_figure():
