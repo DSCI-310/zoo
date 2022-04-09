8d7f635d234fa4c6b8bfc87978a0fc754fc96d27
jossiej00 <88167742+jossiej00@users.noreply.github.com>
2022-04-09 14:24:52 -0700

    update pre_processing

--- src/zoo/pre_processing.py
+++ src/zoo/pre_processing.py
@@ -2,22 +2,26 @@ import pandas as pd
 
 
 # ' download the data from the link without header and add the desired header to the data, as many
-# ' date doesn't have header attached to it
+# '   date doesn't have header attached to it
+# '
+# '
+# ' @param link: the link from the data without header will be downloaded
+# ' @pparam header: the desired header to be added
+# '
+# ' @return return: dataframe contating the data and header
+# '
+# ' @export
+# '
+# ' @examples
+# ' pre_process("https://raw.githubusercontent.com/jossiej00/iris_data/main/iris_raw.csv",
+# '              ["Sepal.Length", "Sepal.Width", "Petal.Width", "Petal.Length", "Species"])
 
-# ' param link: the link from the data without header will be downloaded
-# ' param header: the desired header to be added
-# ' return: dataframe contating the data and header
 def pre_process(link, header):
-    """
-    download the data from the link without header and add the desired header to the data, as many
-    date doesn't have header attached to it
 
-    :param link: the link from the data without header will be downloaded
-    :param header: the desired header to be added
-    :return: dataframe contating the data and header
-    """
+    if isinstance(link, str) & isinstance(header, list):
+        data = pd.read_csv(link, header=None)
+        data.columns = header
+        return data
+    else:
+        return ("Input 'link' should be a string type web link and input 'header' should be a list!")
 
-    data = pd.read_csv(link, header=None)
-    data.columns = header
-
-    return data
--- tests/test_pre_processing.py
+++ tests/test_pre_processing.py
@@ -1,23 +1,38 @@
-from src.zoo.pre_processing import *
+from src.pre_processing import *
 import pandas as pd
 
+# test two expected valid cases
 link = "https://raw.githubusercontent.com/poddarswakhar/dump/main/zoo.data"
 header = ["animalName", "hair", "feathers", "eggs", "milk", "airborne", "aquatic",
           "predator", "toothed", "backbone", "breathes", "venomous", "fins",
           "legs", "tail", "domestic", "catsize", "type"]
 
+link2 = "https://raw.githubusercontent.com/jossiej00/iris_data/main/iris_raw.csv"
+header2 = ["Sepal.Length", "Sepal.Width", "Petal.Width", "Petal.Length", "Species"]
 # expected df
 expected = pd.read_csv("https://raw.githubusercontent.com/poddarswakhar/dump/main/exp.csv")
+expected2 = pd.read_csv("https://raw.githubusercontent.com/jossiej00/iris_data/main/iris.csv")
 
-
+## test one invalid input case
+invalid_header = 2
 def test_pre_process():
+    ## test two expected valid cases
     actual = pre_process(link, header)
+    actual2 = pre_process(link2,header2)
 
     # checking the colm and row
     act_shape = actual.shape
     exp_shape = expected.shape
 
+    act_shape2 = actual2.shape
+    exp_shape2 = expected2.shape
+
     if act_shape == exp_shape:
+        assert True
+    else:
+        assert False
+
+    if act_shape2 == exp_shape2:
         assert True
     else:
         assert False
@@ -25,5 +40,17 @@ def test_pre_process():
     # checking the content of actual and expected
     if actual.equals(expected):
         assert True
+    else:
+        assert False
+
+    if actual2.equals(expected2):
+        assert True
+    else:
+        assert False
+
+    ## test if warning msg pops up
+    msg = pre_process(link, invalid_header)
+    if msg == "Input 'link' should be a string type web link and input 'header' should be a list!":
+        assert  True
     else:
         assert False
