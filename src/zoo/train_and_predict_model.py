e4386c88aac8f55d4b77f931c4346207c25b99e7
poddarswakhar <poddarswakhar97@gmail.com>
2022-04-09 14:57:29 -0700

    FM: Feedback addressing

--- src/zoo/train_and_predict_model.py
+++ src/zoo/train_and_predict_model.py
@@ -8,75 +8,104 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 
 
-
-# ' creates the final model of the algorithms specified
-# ' @param algorithm: algorithm of the model
-# ' @param threshold: threshold for the algorthm like depth, n_neighbors
-# ' @param X_train: x train data
-# ' @param X_test: x test data
-# ' @param y_train: y train data
-# ' @param y_test: y test data
-# ' @return a model
-# ' @examples
-# ' final_Model(algorithm, threshold, X_train, X_test, y_train, y_test, X, y)
-
 def final_Model(algorithm, threshold, X_train, X_test, y_train, y_test, X, y):
-
-
+    """
+    creates the final model of the algorithms specified
+    :param algorithm: algorithm of the model
+    :param threshold: threshold for the algorthm like depth, n_neighbors
+    :param X_train: x train data
+    :param X_test: x test data
+    :param y_train: y train data
+    :param y_test: y test data
+    :param X: All X data or Features
+    :param y: All Y data or predictor variable
+    :return: a model
+    """
     if algorithm.lower().strip() == "svm":
-        svec = svm.SVC(kernel='poly')
-        svec.fit(X_train, y_train)
-        yhat = svec.predict(X_test)
-        acc = jaccard_score(y_test, yhat, average='micro')
-        print("SVM INFO: So the Training Jaccard score for SVM is: " + str(acc))
-        print("\nSVM Evaluation:\n")
-        print(classification_report(y_test, yhat))
-        # Final SVM is here used the splited test part to train again for better training, and better prediction
-        svec = svm.SVC(kernel='poly')
-        finalSVM = svec.fit(X, y)
         # returning the final svm model
-        return finalSVM
+        return helper_svm(X_train, X_test, y_train, y_test, X, y)
 
     elif algorithm.lower().strip() == "lr":
-        LR = LogisticRegression(C=0.07, solver='sag').fit(X_train, y_train)
-        yhat = LR.predict(X_test)
-        acc = jaccard_score(y_test, yhat, average='micro')
-        print("LR INFO: So the Training Jaccard score for Logistic Regression is: " + str(acc))
-        print("\nLR Evaluation: \n")
-        print(classification_report(y_test, yhat))
-        # final LR model is here used the splited test part to train again for better training, and better prediction
-        finalLR = LogisticRegression(C=0.07, solver='sag').fit(X, y)
         # returning the final lr model
-        return finalLR
+        return helper_lr(X_train, X_test, y_train, y_test, X, y)
 
     elif algorithm.lower().strip() == "dt":
-        Final_dec_Tree = DecisionTreeClassifier(criterion="entropy", max_depth=threshold)
-        Final_dec_Tree.fit(X, y)
-        yhat = Final_dec_Tree.predict(X_test)
-        accuracyScore = metrics.accuracy_score(y_test, yhat)
-        print("DT INFO: So the accuracy score for max depth = " + str(threshold) + " is " + str(accuracyScore))
-        # cross-validation on decision tree
-        cv_results_dt = cross_validate(Final_dec_Tree, X_train, y_train, cv=4, return_train_score=True);
-        print("\nDT Cross Validate: \n")
-        print(pd.DataFrame(cv_results_dt).mean())
-        print("\nDT Classification report: \n")
-        print(classification_report(y_test, yhat))
-        # returning the final dt model
-        return Final_dec_Tree
+        return helper_dt(threshold, X_train, X_test, y_train, y_test, X, y)
 
     elif algorithm.lower().strip() == "knn":
-        final_knn_model = KNeighborsClassifier(n_neighbors=threshold).fit(X, y)
-        yhat = final_knn_model.predict(X_test)
-        accuracyScore = metrics.accuracy_score(y_test, yhat)
-        print("KNN INFO: So the accuracy score for K = " + str(threshold) + " is " + str(accuracyScore))
-        # cross-validation on knn
-        cv_results_knn = cross_validate(final_knn_model, X_train, y_train, cv=3, return_train_score=True);
-        print("\nKNN Cross Validate: \n")
-        print(pd.DataFrame(cv_results_knn).mean())
-        print("\nKNN Classification report: \n")
-        print(classification_report(y_test, yhat))
         # returning the final kn model
-        return final_knn_model
+        return helper_knn(threshold, X_train, X_test, y_train, y_test, X, y)
 
     else:
         return "Invalid model algorthm or model is not supported yet"
+
+
+def helper_svm(X_train, X_test, y_train, y_test, X, y):
+    """
+    helper method of final_Model for SVM
+    """
+    svec = svm.SVC(kernel='poly')
+    svec.fit(X_train, y_train)
+    yhat = svec.predict(X_test)
+    acc = jaccard_score(y_test, yhat, average='micro')
+    print("SVM INFO: So the Training Jaccard score for SVM is: " + str(acc))
+    print("\nSVM Evaluation:\n")
+    print(classification_report(y_test, yhat))
+    # Final SVM is here used the splited test part to train again for better training, and better prediction
+    svec = svm.SVC(kernel='poly')
+    finalSVM = svec.fit(X, y)
+    # returning the final svm model
+    return finalSVM
+
+
+def helper_dt(threshold, X_train, X_test, y_train, y_test, X, y):
+    """
+    helper method of final_Model for DT
+    """
+    Final_dec_Tree = DecisionTreeClassifier(criterion="entropy", max_depth=threshold)
+    Final_dec_Tree.fit(X, y)
+    yhat = Final_dec_Tree.predict(X_test)
+    accuracyScore = metrics.accuracy_score(y_test, yhat)
+    print("DT INFO: So the accuracy score for max depth = " + str(threshold) + " is " + str(accuracyScore))
+    # cross-validation on decision tree
+    cv_results_dt = cross_validate(Final_dec_Tree, X_train, y_train, cv=4, return_train_score=True);
+    print("\nDT Cross Validate: \n")
+    print(pd.DataFrame(cv_results_dt).mean())
+    print("\nDT Classification report: \n")
+    print(classification_report(y_test, yhat))
+    # returning the final dt model
+    return Final_dec_Tree
+
+
+def helper_lr(X_train, X_test, y_train, y_test, X, y):
+    """
+    helper method of final_Model for LR
+    """
+    LR = LogisticRegression(C=0.07, solver='sag').fit(X_train, y_train)
+    yhat = LR.predict(X_test)
+    acc = jaccard_score(y_test, yhat, average='micro')
+    print("LR INFO: So the Training Jaccard score for Logistic Regression is: " + str(acc))
+    print("\nLR Evaluation: \n")
+    print(classification_report(y_test, yhat))
+    # final LR model is here used the splited test part to train again for better training, and better prediction
+    finalLR = LogisticRegression(C=0.07, solver='sag').fit(X, y)
+    # returning the final lr model
+    return finalLR
+
+
+def helper_knn(threshold, X_train, X_test, y_train, y_test, X, y):
+    """
+    helper method of final_Model for KNN
+    """
+    final_knn_model = KNeighborsClassifier(n_neighbors=threshold).fit(X, y)
+    yhat = final_knn_model.predict(X_test)
+    accuracyScore = metrics.accuracy_score(y_test, yhat)
+    print("KNN INFO: So the accuracy score for K = " + str(threshold) + " is " + str(accuracyScore))
+    # cross-validation on knn
+    cv_results_knn = cross_validate(final_knn_model, X_train, y_train, cv=3, return_train_score=True);
+    print("\nKNN Cross Validate: \n")
+    print(pd.DataFrame(cv_results_knn).mean())
+    print("\nKNN Classification report: \n")
+    print(classification_report(y_test, yhat))
+    # returning the final kn model
+    return final_knn_model
